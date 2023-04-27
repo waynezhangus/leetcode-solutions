8d6786f536cc1701905a9ac08a8dcbda5c9d88aa
Wayne Zhang <waynezhangus@gmail.comwq>
2023-04-27 13:32:58 -0700

    tree

---
+++ JS/589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+    let st = [];
+    let res = [];
+    if (root) st.push(root);
+    while (st.length) {
+        let cur = st.pop();
+        if (cur) {
+            st.push(cur);
+            st.push(null);
+            if (cur.children) cur.children.forEach(child => {st.push(child)}); 
+        } else {
+            let node = st.pop();
+            res.push(node.val);
+        }
+    }
+    return res.reverse();
+};
